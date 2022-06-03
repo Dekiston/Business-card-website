8bc8559fed53ac91238b78c705a00b0e7a90b523
dollino <76242905+Dekiston@users.noreply.github.com>
2022-06-03 21:58:59 +0300

    The first concept of the site (infinite)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 
+import {Carusel} from './Carousel.js';
 import './App.scss';
 import logo from './/components/logo.jpg';
+import { BsFillBrightnessHighFill, BsFillBrightnessLowFill } from "react-icons/bs";
+
+
+const TeleportAbout = () => {console.log ('Должно телепортировать на X = -100')}
+const TeleportSkills = () => {console.log ('Должно телепортировать на X = -200')}
+const TeleportContacts = () => {console.log ('Должно телепортировать на X = -300')}
 
 function App() {
   return (
     <div className="App">
-      <header id="header">
+      
+      <header>
       <div>
-        <img src={logo} class="logo" alt="Здесь должна быть картинка"></img>
+        <img src={logo} className="logo" alt="Здесь должна быть картинка"></img>
       </div>
-      <div class = "about">
-        <h1>Обо мне</h1>
-        <h1>Мои навыки</h1>
-        <h1>Контакты</h1>
-        <input type="checkbox" class="switch"></input>
+      <div className= "about">
+        <h1 className='Teleport' onClick={TeleportAbout}>Обо мне</h1>
+        <h1 className='Teleport' onClick={TeleportSkills}>Мои навыки</h1>
+        <h1 className='Teleport' onClick={TeleportContacts}>Контакты</h1>
       </div>
+      <BsFillBrightnessHighFill id='Day'/>
+      <BsFillBrightnessLowFill id='Night'/>
       </header>
+
     <body>
-       <div>
-         <h1>Ger</h1>
+       <div className= 'carousel'>
+         <Carusel>
+           <div className='contactsPages'><h1>Здесь будут мои контакты</h1></div>
+           <div className='aboutPages'><h1>Здесь будет информация обо мне</h1></div>
+           <div className='skillsPages'><h1>Здесь будут мои навыки</h1></div>
+           <div className='contactsPages'><h1>Здесь будут мои контакты</h1></div>
+           <div className='aboutPages'><h1>Здесь будет информация обо мне</h1></div>
+         </Carusel>
         </div>
     </body>
+
     </div>
   );
 }
---
+++ src/Carousel.js
@@ -0,0 +1,57 @@
+import { useState, useEffect, Children, cloneElement } from 'react';
+import './App.scss';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
+export const Carusel = ({children}) => {
+
+    const [pages, setPages] = useState ([]);
+    const [offset, setOffset] = useState ([]);
+    
+    const LeftArrowClick = () => {
+        setOffset((currentOffset) => {
+            if (currentOffset === -100) { return -400}
+            const newOffset = currentOffset + 100;
+            console.log (newOffset);
+            return newOffset
+        })
+    }
+    
+    const RightArrowClick = () => {         
+        setOffset((currentOffset) => {
+            if (currentOffset === -300) { return 0}
+            const newOffset = currentOffset - 100;
+            console.log ('Правый =' + newOffset);
+            
+            return newOffset
+        })
+    }
+
+    useEffect(() => {
+        setPages(
+            Children.map(children, child => {
+                return cloneElement(child, { style: { minHeight: '100%' ,maxHeight: '100%', minWidth: '100%', maxWidth: '100%', },})
+            })
+        )
+    }, []);
+
+    return (
+        <div className='main-window'>
+            <div className="window">
+            {(offset === 0) || (offset === -400)  ? (
+                <div className="allPages" style = {{transform: `translateX(${offset}%)` }}>                
+                    {pages}
+                </div> ) :
+                <div className="allPagesAnimation" style = {{transform: `translateX(${offset}%)` }}>                
+                {pages}
+            </div>
+            }
+            </div>
+            <FaChevronLeft className='arrow-left' onClick={LeftArrowClick}/>
+            <FaChevronRight className='arrow-right' onClick={RightArrowClick}/>
+        </div>
+        
+    )
+    
+}
+
+
